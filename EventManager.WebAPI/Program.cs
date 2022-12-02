a730810b21373b47045ce9ffb1baf926c58c7cba
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2022-12-02 16:34:38 +0100

    Config dependency injection

--- EventManager.WebAPI/Program.cs
+++ EventManager.WebAPI/Program.cs
@@ -1,11 +1,32 @@
+using EventManager.BLL.Interfaces;
+using EventManager.BLL.Services;
+using EventManager.DAL.Interfaces;
+using EventManager.DAL.Repositories;
+using System.Data;
+using System.Data.SqlClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IDbConnection>(service =>
+{
+    string connectionString = builder.Configuration.GetConnectionString("Default");
+    return new SqlConnection(connectionString);
+});
+
+builder.Services.AddTransient<IMemberRepository, MemberRepository>();
+builder.Services.AddTransient<IActivityRepository, ActivityRepository>();
+builder.Services.AddTransient<IRegistrationRepository, RegistrationRepository>();
+
+builder.Services.AddTransient<IMemberService, MemberService>();
+builder.Services.AddTransient<IActivityService, ActivityService>();
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
