f3b3ba6bb8559969d665dc8fa4604d9fc79667b3
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2022-12-06 03:08:32 +0100

    Auth Controller

---
+++ EventManager.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,74 @@
+﻿using EventManager.BLL.Interfaces;
+using EventManager.Domain.Entities;
+using EventManager.WebAPI.DataTransferObjects;
+using EventManager.WebAPI.Helpers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventManager.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        IMemberService _MemberService;
+        TokenService _TokenService;
+
+        public AuthController(IMemberService memberService, TokenService tokenService)
+        {
+            _MemberService = memberService;
+            _TokenService = tokenService;
+        }
+
+
+        [HttpPost("Login")]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] AuthLoginDTO loginDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Member? member = _MemberService.Login(loginDTO.Identifier, loginDTO.Password);
+
+            if(member is null)
+            {
+                return Problem(
+                    detail: "You have entered either the Username and/or Password incorrectly.",
+                    statusCode: StatusCodes.Status400BadRequest
+                ) ;
+            }
+
+            return Ok(new AuthTokenDTO()
+            {
+                Token = _TokenService.GenerateJwt(member)
+            });
+        }
+
+        [HttpPost("Register")]
+        [AllowAnonymous]
+        public IActionResult Register([FromBody] AuthRegisterDTO registerDTO)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Member? member = _MemberService.Register(new Member()
+            {
+                Pseudo = registerDTO.Pseudo,
+                Email = registerDTO.Email,
+                HashPwd = registerDTO.Password
+            });
+
+            if (member is null)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ EventManager.WebAPI/DataTransferObjects/AuthDTO.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManager.WebAPI.DataTransferObjects
+{
+    public class AuthLoginDTO
+    {
+        [Required]
+        public string Identifier { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class AuthRegisterDTO
+    {
+        [Required]
+        [StringLength(50)]
+        public string Pseudo { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [RegularExpression("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).{5,}")]
+        public string Password { get; set; }
+    }
+
+    public class AuthTokenDTO
+    {
+        public string Token { get; set; }
+    }
+}
