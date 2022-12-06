fcdeb08f1dfca65841db6279db51c0b72dd93b2f
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2022-12-06 09:49:19 +0100

    Optional image for Activity

--- EventManager.Domain/Entities/Activity.cs
+++ EventManager.Domain/Entities/Activity.cs
@@ -7,8 +7,8 @@
 		public string Description { get; set; }
 		public DateTime StartDate { get; set; }
 		public DateTime EndDate { get; set; }
-		public string ImageName { get; set; }
-		public string ImageSrc { get; set; }
+		public string? ImageName { get; set; }
+		public string? ImageSrc { get; set; }
 		public int? MaxGuest { get; set; }
 		public int CreatorId { get; set; }
 		public bool IsCancel { get; set; }
